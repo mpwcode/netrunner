ff28e983bcc6771e878db703fa1237fc28933a2a
mpwcode <>
2022-12-15 14:53:41 +1100

    Testing deploy

---
+++ html/index.php
@@ -0,0 +1,3 @@
+<?php include 'footer.php'; ?>
+blahblahblah
+<?php include 'footer.php' ?>
---
+++ html/pages/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Netrunner</title>
+    <link rel="stylesheet" href="../css/netrunnerfont.css">
+    <link rel="stylesheet" href=".../css/bootstrap.css">
+    <script type="text/javascript" src="../js/bootstrap.js"> 
+    </script>
+  </head>
+  <body>
